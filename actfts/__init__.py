f0c4de285a4c84437e2e47a55fef4a26a0541838
SergioFinances <sergiochess95@gmail.com>
2024-11-18 13:36:16 -0500

    Version 0.1.0
    Version 0.1.0

--- actfts/__init__.py
+++ actfts/__init__.py
@@ -1,1 +1,4 @@
 from actfts.actfts_fun import acfinter
+from actfts.Datasets import DPIEEUU_dataset
+from actfts.Datasets import GDPEEUU_dataset
+from actfts.Datasets import PCECEEUU_dataset
--- actfts/__main__.py
+++ actfts/__main__.py
@@ -1,11 +1,17 @@
 import logging
 
 from actfts.actfts_fun import acfinter
+from actfts.Datasets import DPIEEUU_dataset
+from actfts.Datasets import GDPEEUU_dataset
+from actfts.Datasets import PCECEEUU_dataset
 
 logging.basicConfig(level=logging.INFO)
 
 def main():
     logging.info(acfinter())
+    logging.info(DPIEEUU_dataset())
+    logging.info(GDPEEUU_dataset())
+    logging.info(PCECEEUU_dataset())
 
 if __name__ == '__main__':
     logging.debug('>>> We are starting the execution of the package.')
