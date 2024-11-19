e56188e0c0ad6def9c60add27263938085a90af6
SergioFinances <66971946+SergioFinances@users.noreply.github.com>
2024-11-19 11:31:28 -0500

    update
    Version 0.1.0

--- actfts/__main__.py
+++ actfts/__main__.py
@@ -1,5 +1,6 @@
 import logging
 
+import pandas as pd
 from actfts.actfts_fun import acfinter
 from actfts.Datasets import DPIEEUU_dataset
 from actfts.Datasets import GDPEEUU_dataset
@@ -8,7 +9,9 @@ from actfts.Datasets import PCECEEUU_dataset
 logging.basicConfig(level=logging.INFO)
 
 def main():
-    logging.info(acfinter())
+    datag = DPIEEUU_dataset()
+    x = datag['DPIEEUU']
+    logging.info(acfinter(datag = x))
     logging.info(DPIEEUU_dataset())
     logging.info(GDPEEUU_dataset())
     logging.info(PCECEEUU_dataset())
