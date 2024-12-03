24da7e3d28485777e18d952819a77e549f2d953e
SergioFinances <sergiochess95@gmail.com>
2024-12-03 18:06:17 -0500

    Update setup.py
    Version 0.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-VERSION = '0.1.13'
+VERSION = '0.1.15'
 DESCRIPTION = 'The Autocorrelation Tools Featured for Time Series actfts package simplifies time series analysis by providing tools for ACF, PACF, and stationarity tests with dynamic, interactive visualizations. It validates and preprocesses data, computes ACF/PACF for multiple lags, and performs tests like Box-Pierce, Ljung-Box, ADF, KPSS, and PP. Results are organized into tables, exportable as TIFF or Excel files, with an interactive mode for on-screen visualization.'
 PACKAGE_NAME = 'actfts'
 AUTHOR = ['Sergio Andrés Sierra Luján', 'David Esteban Rodríguez Guevara']
@@ -34,10 +34,16 @@ setup(
         'scipy',
         'pandas',
         'Flask>=1.0.0',
-        'Werkzeug>=3.0.0',
+        'Werkzeug==2.0.3',
         'plotly',
         'openpyxl',
         'dash',
+        'python-dateutil>=2.8.2',
+        'pytz>=2020.1',
+        'tzdata>=2022.7',
+        'patsy>=0.5.6',
+        'packaging>=21.3',
+        'six>=1.5',
         'arch'
     ],
     classifiers=[
